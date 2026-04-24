6ea5fd98960979d3a406686edbeeb7fb4c2e63cc
Vladimir Yazadzhiev <vladimir@safeex.com>
2026-04-24 15:18:25 +0200

    DEPLOYMENT: Disable GH Pages deployment in favor of Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // Use a different base path when deployed on Vercel vs deploying to GH Pages
   base: process.env.VERCEL ? "/" : "/watchlist-app/",
   plugins: [react()],
 });
