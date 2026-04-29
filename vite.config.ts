b297bbf78d5033d70a91467963042cb0d43d38a2
vladdyog <vladimiryazadjiev@gmail.com>
2026-04-29 10:03:39 +0200

    [#28] WIP: Add prettier and eslint config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
-import { defineConfig } from "vite";
-import packageJson from "./package.json";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+import packageJson from './package.json';
 
 export default defineConfig({
-  base: process.env.VERCEL ? "/" : "/watchlist-app/",
+  base: process.env.VERCEL ? '/' : '/watchlist-app/',
   plugins: [react(), tailwindcss()],
   define: {
-    "import.meta.env.PACKAGE_VERSION": JSON.stringify(
-      packageJson.version,
-    ),
-    "import.meta.env.AUTHOR": JSON.stringify(
-      packageJson.author,
-    ),
+    'import.meta.env.PACKAGE_VERSION': JSON.stringify(packageJson.version),
+    'import.meta.env.AUTHOR': JSON.stringify(packageJson.author),
   },
 });
