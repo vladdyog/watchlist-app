3b3bcbb44e14667e03146f2afded1cd3df1ffd73
vladdyog <vladimiryazadjiev@gmail.com>
2026-04-29 14:03:06 +0200

    VERCEL: Enable analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import '../index.css';
 
+import { Analytics } from '@vercel/analytics/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -8,5 +9,6 @@ import App from './App';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 );
