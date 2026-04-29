1194c4e6f914addac5053ec12443dbe135d9bce4
vladdyog <vladimiryazadjiev@gmail.com>
2026-04-29 14:19:48 +0200

    Fix vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import '../index.css';
 
-import { Analytics } from '@vercel/analytics/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -9,6 +8,5 @@ import App from './App';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>,
 );
